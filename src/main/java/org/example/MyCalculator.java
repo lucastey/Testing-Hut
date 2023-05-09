502b54008d4c2042749c27c871374a28ca3f8d1f
Lucas Tay <tayboonkai@gmail.com>
2023-05-09 16:16:07 +0800

    Negative case

--- src/main/java/org/example/MyCalculator.java
+++ src/main/java/org/example/MyCalculator.java
@@ -4,4 +4,8 @@ public class MyCalculator {
     public static int add(int a, int b){
         return a + b;
     }
+
+    public static int multiply(int a, int b){
+        return a/b;
+    }
 }
--- src/test/java/MyCalculatorTest.java
+++ src/test/java/MyCalculatorTest.java
@@ -10,4 +10,13 @@ public class MyCalculatorTest {
 
         Assert.assertEquals(expectedResult, actualResult, "The actual result should be 80 as well");
     }
+
+    @Test
+    void testMultiplyMethod(){
+        int firstOperand = 10, secondOperand = 5;
+        int expectedResult = firstOperand * secondOperand;
+        int actualResult = MyCalculator.multiply(firstOperand, secondOperand);
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
 }
